8cf0fd7639c28b82751897af1e48967817bef18f
Jr Zendejas <JrZendejas@Jr-Zendejass-MacBook-Pro-2.local>
2017-03-28 13:42:06 -0700

    ths is what I got so far

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,16 @@
 var serverController = require('./../controllers/server_controller.js')
 
 module.exports = function(app){
-	// Define routes here
+	app.post('/register', serverController.register);
+	app.post('/login', serverController.login);
+	app.get('/logout', serverController.logout);
+	app.get('/current', serverController.current);
+	app.get('/messages', serverController.getMessages);
+	app.post('/message', serverController.createMessage);
+	app.delete('/message_id', serverController.delete);
+	app.get('comment', serverController.comment);
+	app.post('comment', serverController.comment);
+	
 }
+
+
