0f1c9b97df0052c86be0bc4c28f73248cd5c7ef6
Jr Zendejas <JrZendejas@Jr-Zendejass-MacBook-Pro-2.local>
2017-03-28 15:45:06 -0700

    this is the new code

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 var path = require('path');
 var fs = require('fs');
 
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/mini-store');
+mongoose.connect('mongodb://localhost/MEANProjectWeek');//name db file
 
 var models_path = path.join(__dirname, '../models/');
 
